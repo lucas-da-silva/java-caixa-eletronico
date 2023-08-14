6b1170442fb6684813af07ecbeee1dc196970d80
Lucas da Silva <lucassouza00012345@gmail.com>
2023-08-14 17:56:45 -0300

    test: implementa testes para a classe Transacao

--- src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -1,40 +1,44 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Testes dos métodos da classe Transacao")
 class TransacaoTest {
 
+  private static final double QUANTIA = 100.00;
+  private static final String DESCRICAO = "Saque";
 
   @Test
   @DisplayName("21 - Testa o método construtor da classe Transacao.")
   void construtorTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(QUANTIA, DESCRICAO);
+    assertEquals(QUANTIA, transacao.getQuantia());
+    assertEquals(DESCRICAO, transacao.getDescricao());
   }
 
 
   @Test
   @DisplayName("22 - Testa o método Getter do atributo quantia.")
   void getQuantiaTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(QUANTIA, DESCRICAO);
+    assertEquals(QUANTIA, transacao.getQuantia());
   }
 
   @Test
   @DisplayName("23 - Testa o método retornar resumo transacao.")
   void retornarResumoTransacaoTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(QUANTIA, DESCRICAO);
+    assertEquals(DESCRICAO, transacao.getDescricao());
   }
 
   @Test
   @DisplayName("24 - Testa o método instante está gerando o instante corretamente.")
   void retornarInstanteTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(QUANTIA, DESCRICAO);
+    assertEquals(transacao.retornarInstante(), transacao.getInstante());
   }
 
 }
