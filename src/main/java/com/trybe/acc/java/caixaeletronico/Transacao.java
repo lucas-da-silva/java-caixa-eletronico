63b0a581f2df6ed1e3976585f21ccb95f044e06b
Lucas da Silva <lucassouza00012345@gmail.com>
2023-08-16 09:34:02 -0300

    test: implementa a classe Transacao e seus métodos

--- src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
@@ -1,6 +1,59 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Transacao {
+
+  private double quantia;
+  private String instante;
+  private String descricao;
+  private Conta conta;
+
+  /**
+   * Construtor da classe Transacao.
+   *
+   * @param quantia   Quantia da transação.
+   * @param descricao Descrição da transação.
+   */
+  public Transacao(double quantia, String descricao) {
+    this.quantia = quantia;
+    this.descricao = descricao;
+    this.instante = this.retornarInstante();
+  }
+
+  public double getQuantia() {
+    return this.quantia;
+  }
+
+  /**
+   * Retorna o resumo da transação.
+   *
+   * @return Resumo da transação.
+   */
+  public String retornarResumoTransacao() {
+    String iconeTransacao;
+
+    if (this.descricao.contains("Saque") || this.descricao.contains("Transferência enviada")) {
+      iconeTransacao = " -";
+    } else {
+      iconeTransacao = " +";
+    }
+
+    return this.instante + " -------- " + this.descricao + ": R$ " + quantia + iconeTransacao;
+  }
+
+  public String retornarInstante() {
+    return DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").format(LocalDateTime.now());
+  }
+
+  public String getDescricao() {
+    return this.descricao;
+  }
+
+  public String getInstante() {
+    return this.instante;
+  }
 
 }
 
