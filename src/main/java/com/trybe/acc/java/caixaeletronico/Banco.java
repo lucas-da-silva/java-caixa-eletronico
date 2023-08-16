900224164a4971a15380ef5016d8ba1d0fb559f8
Lucas da Silva <lucassouza00012345@gmail.com>
2023-08-16 09:34:41 -0300

    test: implementa a classe Banco e seus métodos

--- src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
@@ -1,5 +1,141 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Banco {
+
+  private ArrayList<PessoaCliente> pessoasClientes = new ArrayList<>();
+  private ArrayList<Conta> contas = new ArrayList<>();
+
+  /**
+   * Gera um número aleatório e único de 10 dígitos e verifica se ele já existe no banco de dados.
+   *
+   * @return Número aleatório e único de 10 dígitos.
+   */
+  public String gerarNumeroNovaConta() {
+    String numero = this.gerarNumeroAleatorio();
+
+    while (true) {
+      boolean numeroJaExiste = false;
+
+      for (Conta conta : this.contas) {
+        if (conta.getIdConta().equals(numero)) {
+          numeroJaExiste = true;
+          break;
+        }
+      }
+
+      if (numeroJaExiste) {
+        numero = this.gerarNumeroAleatorio();
+      } else {
+        break;
+      }
+    }
+
+    return numero;
+  }
+
+  private String gerarNumeroAleatorio() {
+    StringBuilder numero = new StringBuilder();
+
+    for (int i = 0; i < 10; i++) {
+      int digito = new Random().nextInt(10);
+      numero.append(digito);
+    }
+
+    return numero.toString();
+  }
+
+  /**
+   * Adiciona uma nova pessoa cliente ao banco de dados.
+   *
+   * @param nome  Nome da pessoa cliente.
+   * @param cpf   CPF da pessoa cliente.
+   * @param senha Senha da pessoa cliente.
+   * @return A pessoa cliente adicionada.
+   */
+  public PessoaCliente adicionarPessoaCliente(String nome, String cpf, String senha) {
+    PessoaCliente pessoaCliente = new PessoaCliente(nome, cpf, senha);
+    pessoasClientes.add(pessoaCliente);
+    return pessoaCliente;
+  }
+
+  /**
+   * Adiciona uma nova conta ao banco de dados.
+   *
+   * @param tipo    Tipo da conta: "Corrente" ou "Poupança".
+   * @param cliente Pessoa cliente que está criando a conta.
+   */
+  public void adicionarConta(String tipo, PessoaCliente cliente) {
+    Conta novaConta = new Conta(tipo, cliente, this);
+    cliente.adicionarConta(novaConta);
+    this.contas.add(novaConta);
+  }
+
+  /**
+   * Realiza o login de uma pessoa cliente.
+   *
+   * @param cpf   CPF da pessoa cliente.
+   * @param senha Senha da pessoa cliente.
+   * @return A pessoa cliente que realizou o login.
+   */
+  public PessoaCliente pessoaClienteLogin(String cpf, String senha) {
+    for (PessoaCliente pessoaCliente : pessoasClientes) {
+      if (pessoaCliente.getCpf().equals(cpf) && pessoaCliente.validarSenha(senha)) {
+        return pessoaCliente;
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Transfere fundos de uma conta para outra.
+   *
+   * @param pessoaCliente Pessoa cliente que está realizando a transferência.
+   * @param daConta       Número da conta de onde os fundos serão retirados.
+   * @param paraConta     Número da conta para onde os fundos serão enviados.
+   * @param valor         Valor a ser transferido.
+   */
+
+  public void transferirFundos(PessoaCliente pessoaCliente, int daConta, int paraConta,
+      double valor) {
+    Conta contaDebitada = pessoaCliente.retornarContaEspecifica(daConta);
+    Conta contaCreditada = pessoaCliente.retornarContaEspecifica(paraConta);
+
+    contaDebitada.adicionarTransacao(valor, "Transferência enviada");
+    contaCreditada.adicionarTransacao(valor, "Transferência recebida");
+  }
+
+  public void sacar(PessoaCliente pessoaCliente, int daConta, double quantia) {
+    Conta clienteConta = pessoaCliente.retornarContaEspecifica(daConta);
+    clienteConta.adicionarTransacao(quantia, "Saque");
+  }
+
+  public void depositar(PessoaCliente pessoaCliente, int paraConta, double quantia) {
+    Conta contaDeposito = pessoaCliente.retornarContaEspecifica(paraConta);
+    contaDeposito.adicionarTransacao(quantia, "Depósito recebido");
+  }
+
+  /**
+   * Mostra o extrato de uma conta.
+   *
+   * @param pessoaCliente Pessoa cliente que está solicitando o extrato.
+   * @param conta         Número da conta que terá o extrato mostrado.
+   */
+  public void mostrarExtrato(PessoaCliente pessoaCliente, int conta) {
+    Conta contaExtrato = pessoaCliente.retornarContaEspecifica(conta);
+    System.out.println("\n\nExtrato da conta " + contaExtrato.getIdConta());
+    contaExtrato.retornarExtrato();
+  }
+
+  public ArrayList<PessoaCliente> getPessoasClientes() {
+    return this.pessoasClientes;
+  }
+
+  public ArrayList<Conta> getContas() {
+    return this.contas;
+  }
 
 }
--- src/main/java/com/trybe/acc/java/caixaeletronico/CaixaEletronico.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/CaixaEletronico.java
@@ -1,5 +1,221 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.util.Scanner;
+
 public class CaixaEletronico {
 
+  /**
+   * Método principal do programa.
+   *
+   * @param args Argumentos passados para o programa.
+   */
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    Banco banco = new Banco();
+
+    PessoaCliente pessoaCliente1 =
+        banco.adicionarPessoaCliente("Alexiania Pereira", "842.074.410-77", "1234");
+    banco.adicionarConta("Poupança", pessoaCliente1);
+    banco.adicionarConta("Corrente", pessoaCliente1);
+
+    PessoaCliente pessoaCliente2 =
+        banco.adicionarPessoaCliente("Abadiania Silva", "848.725.510-87", "1234");
+    banco.adicionarConta("Poupança", pessoaCliente2);
+    banco.adicionarConta("Corrente", pessoaCliente2);
+
+    PessoaCliente pessoaCliente3 =
+        banco.adicionarPessoaCliente("Camaragibe Oliveira", "433.892.200-11", "1234");
+    banco.adicionarConta("Poupança", pessoaCliente3);
+    banco.adicionarConta("Corrente", pessoaCliente3);
+
+    while (true) {
+
+      System.out.println("\n\nBem-vindo ao Banco da Trybe\n\n");
+      System.out.print("Entre com seu CPF: ");
+      String pessoaClienteCpf = sc.nextLine();
+      System.out.print("Entre com sua senha: ");
+      String senha = sc.nextLine();
+
+      PessoaCliente pessoaClienteAutenticada = banco.pessoaClienteLogin(pessoaClienteCpf, senha);
+
+      if (pessoaClienteAutenticada == null) {
+        System.out.println("\n\nCombinação de CPF e senha incorretos. Tente novamente");
+
+      } else {
+
+        int op;
+
+        do {
+
+          pessoaClienteAutenticada.retornarResumoContas();
+
+          System.out.println("\n\nO que você gostaria de fazer?");
+          System.out.println("  1) Mostrar Extrato");
+          System.out.println("  2) Sacar");
+          System.out.println("  3) Depositar");
+          System.out.println("  4) Transferir");
+          System.out.println("  5) Sair");
+          System.out.println();
+          System.out.print("Entre com sua opção: ");
+
+          op = sc.nextInt();
+
+          if (op < 1 || op > 5) {
+            System.out.println("Opção inválida, escolha uma opção válida.");
+          }
+
+          if (op == 1) {
+
+            int conta;
+
+            do {
+              System.out.printf(
+                  "Entre com o número (1-%d) para a conta\nque " + "o extrato será impresso: ",
+                  pessoaClienteAutenticada.retornaNumeroDeContas());
+              conta = sc.nextInt() - 1;
+              if (conta < 0 || conta >= pessoaClienteAutenticada.retornaNumeroDeContas()) {
+                System.out.println("Número inválido, tente novamente.");
+              } else {
+                break;
+              }
+            } while (true);
+
+            banco.mostrarExtrato(pessoaClienteAutenticada, conta);
+
+
+          } else if (op == 2) {
+
+            int deConta;
+            double quantia;
+            double saldoConta;
+
+            do {
+              System.out.printf("Entre o número (1-%d) para selecionar a conta para " + "o saque: ",
+                  pessoaClienteAutenticada.retornaNumeroDeContas());
+              deConta = sc.nextInt() - 1;
+              if (deConta < 0 || deConta >= pessoaClienteAutenticada.retornaNumeroDeContas()) {
+                System.out.println("Índice de conta inválido, tente novamente.");
+              } else {
+                break;
+              }
+            } while (true);
+
+            saldoConta = pessoaClienteAutenticada.retornarSaldoContaEspecifica(deConta);
+
+            do {
+              System.out.printf("Entre com a quantia a ser sacada (máximo R$%.02f): R$ ",
+                  saldoConta);
+              quantia = sc.nextDouble();
+              if (quantia < 0) {
+                System.out.println("quantia deve ser maior que zero.");
+              } else if (quantia > saldoConta) {
+                System.out.printf("quantia não pode ser maior que o saldo " + "de R$ %.02f.\n",
+                    saldoConta);
+              } else {
+                break;
+              }
+            } while (true);
+
+            banco.sacar(pessoaClienteAutenticada, deConta, quantia);
+
+          } else if (op == 3) {
+
+            int paraConta;
+            double quantia;
+
+            do {
+              System.out.printf(
+                  "Entre com o número (1-%d) para selecionar a conta para " + "depósito: ",
+                  pessoaClienteAutenticada.retornaNumeroDeContas());
+              paraConta = sc.nextInt() - 1;
+
+              if (paraConta < 0 || paraConta >= pessoaClienteAutenticada.retornaNumeroDeContas()) {
+                System.out.println("Índice de conta inválido, tente novamente.");
+
+              } else {
+                break;
+              }
+            } while (true);
+
+            do {
+              System.out.printf("Entre com a quantia de depósito: R$ ");
+              quantia = sc.nextDouble();
+
+              if (quantia < 0) {
+                System.out.println("quantia deve ser maior que zero.");
+
+              } else {
+                break;
+              }
+            } while (true);
+
+            banco.depositar(pessoaClienteAutenticada, paraConta, quantia);
+
+          } else if (op == 4) {
+
+            int daConta;
+            int paraConta;
+            double quantia;
+            double saldoConta;
+
+            do {
+              System.out.printf(
+                  "Entre o número (1-%d) para " + "retirar o valor para transferência: ",
+                  pessoaClienteAutenticada.retornaNumeroDeContas());
+              daConta = sc.nextInt() - 1;
+              if (daConta < 0 || daConta >= pessoaClienteAutenticada.retornaNumeroDeContas()) {
+                System.out.println("Índice de conta inválido, tente novamente.");
+              } else {
+                break;
+              }
+            } while (true);
+
+            saldoConta = pessoaClienteAutenticada.retornarSaldoContaEspecifica(daConta);
+
+            do {
+              System.out.printf(
+                  "Entre o número (1-%d) para "
+                      + "selecionar a conta que receberá a transferência: ",
+                  pessoaClienteAutenticada.retornaNumeroDeContas());
+              paraConta = sc.nextInt() - 1;
+              if (paraConta < 0 || paraConta >= pessoaClienteAutenticada.retornaNumeroDeContas()) {
+                System.out.println("Índice de conta inválido, tente novamente.");
+              } else {
+                break;
+              }
+            } while (true);
+
+            do {
+
+              System.out.printf("Entre com a quantia para ser transferida (máximo R$%.02f): R$ ",
+                  saldoConta);
+              quantia = sc.nextDouble();
+
+              if (quantia < 0) {
+                System.out.println("quantia deve ser maior que zero.");
+
+              } else if (quantia > saldoConta) {
+                System.out.printf(
+                    "quantia não pode ser maior que o valor do saldo " + "de R$.02f.\n",
+                    saldoConta);
+              } else {
+                break;
+              }
+            } while (true);
+
+            banco.transferirFundos(pessoaClienteAutenticada, daConta, paraConta, quantia);
+
+          } else if (op == 5) {
+            System.out.println("Logout realizado com sucesso!");
+            break;
+          }
+
+        } while (true);
+
+      }
+
+    }
+  }
 }
